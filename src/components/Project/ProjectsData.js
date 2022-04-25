d5ed0f7871726fb7c86e5ed4faaaca7df2706d70
Cristian G <cristianandresgv03@gmail.com>
2022-04-25 11:52:55 -0500

    adding scent zone project

--- src/components/Project/ProjectsData.js
+++ src/components/Project/ProjectsData.js
@@ -1,22 +1,25 @@
 const projectsData = [
+  {
+    image: "./crud.png",
+    technologies: "Node.js - Html - CSS - Javascript",
+    link: "https://mercadoliebre-crud.herokuapp.com/",
+  },
+  {
+    image: "./onepage.png",
+    technologies: "Html - CSS - Javascript",
+    link: "https://cristianagv.github.io/ModernWebsiteProject/",
+  },
 
-    {
-        image: './crud.png',
-        technologies: 'Node.js - Html - CSS - Javascript',
-        link: 'https://mercadoliebre-crud.herokuapp.com/'
-    },
-    {
-        image: './onepage.png',
-        technologies: 'Html - CSS - Javascript',
-        link: 'https://cristianagv.github.io/ModernWebsiteProject/'
-    },
+  {
+    image: "./tesla.jpg",
+    technologies: "Html - CSS - React.js",
+    link: "https://clone-tesla-app.netlify.app/",
+  },
+  {
+    image: "./tiny-logo.png",
+    technologies: "Html - CSS - Javascript - Node.js",
+    link: "https://scent-zone.herokuapp.com/",
+  },
+];
 
-    {
-        image: './tesla.jpg',
-        technologies: 'Html - CSS - React.js',
-        link: 'https://clone-tesla-app.netlify.app/'
-    }
-
-]
-
-export default projectsData
+export default projectsData;
